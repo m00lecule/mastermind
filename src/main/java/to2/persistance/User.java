6437b09ce868221be1f0b608b53d98058135ef61
dygas <!Lukasz222>
2020-01-11 21:39:04 +0100

    refactor

--- src/main/java/to2/persistance/User.java
+++ src/main/java/to2/persistance/User.java
@@ -13,10 +13,19 @@ public class User {
 
     private String email;
     private String nickname;
+    private boolean sendNotification = true;
 
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private List<GameScore> games;
 
+    @Column(name = "send_notification", nullable = false)
+    public boolean isSendNotification() {
+        return sendNotification;
+    }
+
+    public void setSendNotification(boolean sendNotification) {
+        this.sendNotification = sendNotification;
+    }
 
     @Column(name = "nickname", nullable = false, unique = true)
     public String getNickname() {
@@ -37,12 +46,21 @@ public class User {
         this.email = email;
     }
 
+    public List<GameScore> getGames() {
+        return games;
+    }
+
+    public void setGames(List<GameScore> games) {
+        this.games = games;
+    }
+
     public User() {
     }
 
-    public User(String nickname, String email) {
+    public User(String nickname, String email, boolean sendNotification) {
         this.nickname = nickname;
         this.email = email;
+        this.sendNotification = sendNotification;
     }
 }
 
